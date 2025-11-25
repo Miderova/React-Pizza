44f9bcf930b8330b26810297e664939f26bae1d1
Vika Miderova <Slarkmidd@mail.ru>
2025-11-25 19:33:38 +0300

    Add react-pizza as regular folder

---
+++ react-pizza/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import './scss/app.scss'
+import Header from "./components/Header.jsx";
+import Categories from "./components/Categories.jsx";
+import Sort from "./components/Sort.jsx";
+import PizzaBlock from "./components/PizzaBlock.jsx";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title="Мексиканская" price="500" />
+            <PizzaBlock title="Cмешная" price="420" />
+            <PizzaBlock title="Сырная" price="440" />
+            <PizzaBlock title="Пепкиронки" price="330" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
